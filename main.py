33f0db9627d033b8c309bc917acc3ee4bdb2c92a
seara2k <seara2k@users.noreply.github.com>
2020-04-08 14:04:00 +0300

    hard

--- main.py
+++ main.py
@@ -6,164 +6,155 @@ import numpy as np
 
 
 def button_function(button_name):
-    if (len(button_name) == 11):
-        button_number = button_name[len(button_name) - 1]
-    else:
-        button_number = button_name[
-            len(button_name) - 2] + button_name[len(button_name) - 1]
-    if getattr(ui, button_name).isChecked():
-        input = ui.labelresult.text()
-        output = input[0:int(button_number) - 1] + "1" + \
-            input[int(button_number):35]
-        ui.labelresult.setText(output)
-    else:
-        input = ui.labelresult.text()
-        output = input[0:int(button_number) - 1] + "0" + \
-            input[int(button_number):35]
-        ui.labelresult.setText(output)
+	if (len(button_name) == 11):
+		button_number = button_name[len(button_name) - 1]
+	else:
+		button_number = button_name[
+			len(button_name) - 2] + button_name[len(button_name) - 1]
+	if getattr(ui, button_name).isChecked():
+		input = ui.labelresult.text()
+		output = input[0:int(button_number) - 1] + "1" + \
+			input[int(button_number):35]
+		ui.labelresult.setText(output)
+	else:
+		input = ui.labelresult.text()
+		output = input[0:int(button_number) - 1] + "0" + \
+			input[int(button_number):35]
+		ui.labelresult.setText(output)
 
 
 def clear():
-    for i in range(35):
-        button_name = 'pushButton' + str(i + 1)
-        if getattr(ui, button_name).isChecked():
-            getattr(ui, button_name).click()
-    ui.console.clear()
-    ui.console.append("Cleared")
-    QtTest.QTest.qWait(1000)
-    ui.console.clear()
+	for i in range(35):
+		button_name = 'pushButton' + str(i + 1)
+		if getattr(ui, button_name).isChecked():
+			getattr(ui, button_name).click()
+	ui.console.clear()
+	ui.label.setText("")
+	ui.console.append("Cleared")
+	QtTest.QTest.qWait(1500)
+	ui.console.clear()
 
 
 def teach(k):
-    if (k[0] == '0'):
-        global neural_network
-        neural_network = NeuralNetwork()
+	if (k[0] == '0'):
 
-        training_inputs = np.array([[0,1,1,1,0,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,0],
-                                    [0,0,1,0,0,
-                                    0,1,1,0,0,
-                                    0,0,1,0,0,
-                                    0,0,1,0,0,
-                                    0,0,1,0,0,
-                                    0,0,1,0,0,
-                                    0,1,1,1,0],
-                                    [0,1,1,1,0,
-                                    0,0,0,0,1,
-                                    0,0,0,0,1,
-                                    0,0,0,1,0,
-                                    0,0,1,0,0,
-                                    0,1,0,0,0,
-                                    1,1,1,1,1],
-                                    [1,1,1,1,1,
-                                    0,0,0,0,1,
-                                    0,0,0,1,0,
-                                    0,0,1,1,0,
-                                    0,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,0],
-                                    [0,0,0,1,0,
-                                     0,0,1,1,0,
-                                     0,1,0,1,0,
-                                     1,0,0,1,0,
-                                     1,1,1,1,1,
-                                     0,0,0,1,0,
-                                     0,0,0,1,0],
-                                    [1,1,1,1,1,
-                                    1,0,0,0,0,
-                                    1,1,1,1,0,
-                                    0,0,0,0,1,
-                                    0,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,0],
-                                    [0,1,1,1,0,
-                                    1,0,0,0,1,
-                                    1,0,0,0,0,
-                                    1,1,1,1,0,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,0],
-                                    [1,1,1,1,1,
-                                    0,0,0,0,1,
-                                    0,0,0,1,0,
-                                    0,0,1,0,0,
-                                    0,1,0,0,0,
-                                    0,1,0,0,0,
-                                    0,1,0,0,0],
-                                    [0,1,1,1,0,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,0,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,0],
-                                    [0,1,1,1,0,
-                                    1,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,1,
-                                    0,0,0,0,1,
-                                    1,0,0,0,1,
-                                    0,1,1,1,0]])
+		training_inputs = np.array([[0, 1, 1, 1, 0,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 0],
+									[0, 0, 1, 0, 0,
+									0, 1, 1, 0, 0,
+									0, 0, 1, 0, 0,
+									0, 0, 1, 0, 0,
+									0, 0, 1, 0, 0,
+									0, 0, 1, 0, 0,
+									0, 1, 1, 1, 0],
+									[0, 1, 1, 1, 0,
+									0, 0, 0, 0, 1,
+									0, 0, 0, 0, 1,
+									0, 0, 0, 1, 0,
+									0, 0, 1, 0, 0,
+									0, 1, 0, 0, 0,
+									1, 1, 1, 1, 1],
+									[1, 1, 1, 1, 1,
+									0, 0, 0, 0, 1,
+									0, 0, 0, 1, 0,
+									0, 0, 1, 1, 0,
+									0, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 0],
+									[0, 0, 0, 1, 0,
+									 0, 0, 1, 1, 0,
+									 0, 1, 0, 1, 0,
+									 1, 0, 0, 1, 0,
+									 1, 1, 1, 1, 1,
+									 0, 0, 0, 1, 0,
+									 0, 0, 0, 1, 0],
+									[1, 1, 1, 1, 1,
+									1, 0, 0, 0, 0,
+									1, 1, 1, 1, 0,
+									0, 0, 0, 0, 1,
+									0, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 0],
+									[0, 1, 1, 1, 0,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 0,
+									1, 1, 1, 1, 0,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 0],
+									[1, 1, 1, 1, 1,
+									0, 0, 0, 0, 1,
+									0, 0, 0, 1, 0,
+									0, 0, 1, 0, 0,
+									0, 1, 0, 0, 0,
+									0, 1, 0, 0, 0,
+									0, 1, 0, 0, 0],
+									[0, 1, 1, 1, 0,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 0,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 0],
+									[0, 1, 1, 1, 0,
+									1, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 1,
+									0, 0, 0, 0, 1,
+									1, 0, 0, 0, 1,
+									0, 1, 1, 1, 0]])
 
-        training_outputs = np.array([[[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
-                                     [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0]],
-                                     [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]],
-                                     [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0]],
-                                     [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]],
-                                     [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0]],
-                                     [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0]],
-                                     [[0, 0, 0, 0, 0, 0, 0, 1, 0, 0]],
-                                     [[0, 0, 0, 0, 0, 0, 0, 0, 1, 0]],
-                                     [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]])
-        ui.progressBar.setRange(0, 19999)
-        ui.console.append("Teaching...")
-        number = 0
-        for j in range(10):
-            for iteration in range(20000):
+		training_outputs = np.array([[[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+									 [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0]],
+									 [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0]],
+									 [[0, 0, 0, 1, 0, 0, 0, 0, 0, 0]],
+									 [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0]],
+									 [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0]],
+									 [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0]],
+									 [[0, 0, 0, 0, 0, 0, 0, 1, 0, 0]],
+									 [[0, 0, 0, 0, 0, 0, 0, 0, 1, 0]],
+									 [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]])
+		ui.progressBar.setRange(0, 19999)
+		ui.console.append("Teaching...")
+		number = 0
+		for j in range(10):
+			for iteration in range(20000):
 
-                number += 0.1
-                ui.progressBar.setValue(number)
-                neural_network.trainpro(training_inputs, training_outputs, j)
+				number += 0.1
+				ui.progressBar.setValue(number)
+				neural_network.trainpro(training_inputs, training_outputs, j)
 
-        #neural_network.train(training_inputs, training_outputs, 20000)
-        ui.label.setText("Weights found")
-        ui.console.append("Weights found")
-        QtTest.QTest.qWait(2000)
-        ui.label.setText(" ")
-        k[0] = '1'
-    else:
-        ui.label.setText("Already teached")
-        ui.console.append("Already teached")
-        QtTest.QTest.qWait(2000)
-        ui.label.setText(" ")
+		# neural_network.train(training_inputs, training_outputs, 20000)
+		ui.console.append("Weights found")
+		k[0] = '1'
+	else:
+		ui.console.append("Already teached")
 
 
 def go(k, neural_network):
-    if (k[0] == '1'):
-        neuro_input = ui.labelresult.text()
-        neuro_input_mass = list(neuro_input)
-        prediction = np.random.random((10))
-        ui.console.append('Chances')
-        for i in range(10):
-            prediction[i] = neural_network.think(np.array(neuro_input_mass), i)
-            outputstr = str(i) + " : %.4f" % prediction[i]
-            ui.console.append(outputstr)
-        neuro_output_max = max(prediction)
-        for j in range(10):
-            if (prediction[j] == neuro_output_max):
-                final = j
-        ui.label.setText(str(final))
-        outputstr = 'Number is ' + str(final)
+    neuro_input = ui.labelresult.text()
+    neuro_input_mass = list(neuro_input)
+    prediction = np.random.random((10))
+    ui.console.append('Chances')
+    for i in range(10):
+        prediction[i] = neural_network.think(np.array(neuro_input_mass), i)
+        outputstr = str(i) + " : %0.2f" % (prediction[i]*100) +'%'
         ui.console.append(outputstr)
+    neuro_output_max = max(prediction)
+    for j in range(10):
+        if (prediction[j] == neuro_output_max):
+            final = j
+    ui.label.setText(str(final))
+    if (k[0] == '1'):    
+        outputstr = 'Number is ' + str(final)
     else:
-        ui.label.setText("Not teached")
-        QtTest.QTest.qWait(2000)
-        ui.label.setText(" ")
+        outputstr = 'Not teached number is ' + str(final)
+    ui.console.append(outputstr)
 # Create application
 
 app = QtWidgets.QApplication(sys.argv)
@@ -176,7 +167,8 @@ ui.setupUi(Form)
 Form.show()
 # variables
 k = list('0')
-
+global neural_network
+neural_network = NeuralNetwork()
 # logic
 ui.pushButton1.toggled.connect(lambda: button_function('pushButton1'))
 ui.pushButton2.toggled.connect(lambda: button_function('pushButton2'))
@@ -219,3 +211,5 @@ ui.teachButton.clicked.connect(lambda: teach(k))
 ui.goButton.clicked.connect(lambda: go(k, neural_network))
 ui.exitButton.clicked.connect(quit)
 sys.exit(app.exec_())
+
+			
